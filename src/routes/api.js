c0893599ff50f77f8bc980fc632b7b8521b83e3d
mark7dev <mcamacho_ce7@live.com.mx>
2018-09-24 20:31:33 -0500

    user configuration

---
+++ src/routes/api.js
@@ -0,0 +1,12 @@
+const { Router } = require('express');
+const app = Router();
+const User = require('../controllers/User');
+
+app.get('/users', User.index);
+app.delete('/users/:userId', User.remove);
+app.post('/auth/signup', User.create);
+app.post('/auth/login', User.login);
+
+
+
+module.exports = app;
